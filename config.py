0363b280aad49127204559171644dc37bfb6ee02
ToltalTouch <micaeldoamaral@gmail.com>
2025-09-09 11:34:57 -0300

    arquivo com as confiigurações necessarias para a execução, ao utilizar em outro codigo, apenas adicione os itens necessarios

---
+++ config.py
@@ -0,0 +1,7 @@
+SHAREPOINT_CONFIG = {
+    "sharepoint_url": "https://anttgov.sharepoint.com/sites/teste/",
+    "target_list_title": "DB de CONTROLE",
+    "client_id": "your-client-id",
+    "tenant_id": "your-tenant-id",
+    "scopes": ["https://anttgov.sharepoint.com/.default"]
+}
